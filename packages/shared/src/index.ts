76d3a5a2ecd5a6bb17627b80115b8abfe535d877
Khoi Cao Minh <khoicaominh.mmt@gmail.com>
2026-07-26 22:48:03 +0700

    feat: initialize shared package with base configurations and domain exports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,5 @@
+export type { ToDoItem } from "./models/todo";
+export type { User } from "./models/user";
 export * from "./models/todo";
 export * from "./models/user";
 export * from "./constants";
